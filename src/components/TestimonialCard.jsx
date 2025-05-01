8454d298a75c1cd675acb979fb92a8bcae37bcb8
Sid127357 <sidharthkaushal369@gmail.com>
2025-05-02 01:43:28 +0530

    Slider navigati0on and other feature enable

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,13 +1,40 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 import { TESTIMONIALS } from "../data/testimonial";
 
 export const TestimonialCard = () => {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        className="mySwiper"
+        loop={true}
+        autoplay={{
+          delay: 3000, // delay in ms between slides
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1280: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+        }}
+      >
         {TESTIMONIALS.map((t) => (
           <SwiperSlide key={t.id}>
             {t.messages.map((message, index) => {
