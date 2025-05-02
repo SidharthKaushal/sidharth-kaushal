9c4d5b80190a229c68e003c7c5706b9830e56681
Sidharth Kaushal <sidharthkaushal369@gmail.com>
2025-05-02 15:13:01 +0530

    Update skills-card-hover.jsx

--- src/components/ui/skills-card-hover.jsx
+++ src/components/ui/skills-card-hover.jsx
@@ -4,80 +4,80 @@ import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
 
 export const HoverEffect = ({ items, className }) => {
-  let [hoveredIndex, setHoveredIndex] = useState(null);
+  let [hoveredIndex, setHoveredIndex] = useState(null);
 
-  return (
-    <div
-      className={cn(
-        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-7",
-        className
-      )}
-    >
-      {items.map((item, idx) => (
-        <div
-          key={idx}
-          className="relative group  block p-2 h-full w-full"
-          onMouseEnter={() => setHoveredIndex(idx)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <AnimatePresence>
-            {hoveredIndex === idx && (
-              <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
-                layoutId="hoverBackground"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.15 },
-                }}
-                exit={{
-                  opacity: 0,
-                  transition: { duration: 0.15, delay: 0.2 },
-                }}
-              />
-            )}
-          </AnimatePresence>
-          <Card>
-            <CardImage>{item.image}</CardImage>
-            <CardTitle>{item.language}</CardTitle>
-          </Card>
-        </div>
-      ))}
-    </div>
-  );
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-7",
+        className
+      )}
+    >
+      {items.map((item, idx) => (
+        <div
+          key={idx}
+          className="relative group  block p-2 h-full w-full"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <Card>
+            <CardImage>{item.image}</CardImage>
+            <CardTitle>{item.language}</CardTitle>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export const Card = ({ className, children }) => {
-  return (
-    <div
-      className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-light border border-transparent dark:border-white/[0.2]  relative z-20",
-        className
-      )}
-    >
-      <div className="relative z-50">
-        <div className="p-4">{children}</div>
-      </div>
-    </div>
-  );
+  return (
+    <div
+      className={cn(
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-light border border-transparent dark:border-white/[0.2]  relative z-20",
+        className
+      )}
+    >
+      <div className="relative z-50">
+        <div className="p-4">{children}</div>
+      </div>
+    </div>
+  );
 };
 export const CardTitle = ({ className, children }) => {
-  return (
-    <p className={cn("mt-5 text-center text-md", className)}>
-      {children}
-    </p>
-  );
+  return (
+    <p className={cn("mt-5 text-center text-md", className)}>
+      {children}
+    </p>
+  );
 };
 
 export const CardImage = ({ className, children, alt }) => {
-  return (
-    <img
-      src={children}
-      alt={alt}
-      className={cn(
-        "w-16 m-auto",
-        className
-      )}
-    ></img>
-  );
+  return (
+    <img
+      src={children}
+      alt={alt}
+      className={cn(
+        "w-16 m-auto",
+        className
+      )}
+    ></img>
+  );
 };
