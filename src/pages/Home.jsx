534235012d5211780014eab640731140d644020b
Sid127357 <sidharthkaushal369@gmail.com>
2025-05-03 02:08:46 +0530

    Remove the smooth cursor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { ExperienceSection } from "../sections/ExperienceSection";
 import { FirstSketch } from "../sections/FirstSketch";
 import { Skills } from "../sections/Skills";
 import { TestimonialSection } from "../sections/TestimonialSection";
-import { SmoothCursor } from "../components/ui/SmoothCursor";
 export const Home = () => {
   return (
     <>
@@ -11,7 +10,6 @@ export const Home = () => {
       <Skills />
       <FirstSketch/>
       <TestimonialSection/>
-      <SmoothCursor/>
     </>
   );
 };
